517d6b1c596f5eac6495f607fbbe7f367f30ea69
msnodeve <msnodeve@gmail.com>
2019-09-24 17:15:32 +0900

    백그라운드 아이콘

--- leap/leap/Form1.Designer.cs
+++ leap/leap/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace leap
 {
-    partial class Form1
+    partial class DbLeapForm
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
@@ -28,20 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DbLeapForm));
+            this.bottomNotifiyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // bottomNotifiyIcon
+            // 
+            this.bottomNotifiyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("bottomNotifiyIcon.Icon")));
+            this.bottomNotifiyIcon.Text = "notifyIcon1";
+            this.bottomNotifiyIcon.Visible = true;
+            this.bottomNotifiyIcon.DoubleClick += new System.EventHandler(this.onDoubleClick);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip1";
+            this.contextMenuStrip.Size = new System.Drawing.Size(94, 26);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // DbLeapForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(755, 571);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(345, 127);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "DbLeapForm";
+            this.Text = "DBLeap";
+            this.Load += new System.EventHandler(this.DbLeapForm_Load);
+            this.contextMenuStrip.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.NotifyIcon bottomNotifiyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
     }
 }
 
--- leap/leap/Form1.cs
+++ leap/leap/Form1.cs
@@ -7,14 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
+using System.Threading;
 
 namespace leap
 {
-    public partial class Form1 : Form
+    public partial class DbLeapForm : Form
     {
-        public Form1()
+        public DbLeapForm()
         {
             InitializeComponent();
+
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+            this.Visible = false;
+            this.bottomNotifiyIcon.Visible = true;
+            bottomNotifiyIcon.ContextMenuStrip = contextMenuStrip;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void DbLeapForm_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("로드 테스트");
+        }
+
+        private void onDoubleClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("아이콘 더블 클릭 테스트");
         }
     }
 }
