8fb5855014bd558260f0be86282c4b25aa2709e8
msnodeve <msnodeve@gmail.com>
2019-09-25 02:12:16 +0900

    ConsoleApp 립 연동 테스트

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leap;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Controller controller = new Controller();
+
+        }
+    }
+}
