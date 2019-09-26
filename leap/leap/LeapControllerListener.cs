f3bfa965a3185cf778ced67810b98be4d726640b
PresentJay <presentj94@gmail.com>
2019-09-27 04:19:11 +0900

    conflict test

--- leap/leap/LeapControllerListener.cs
+++ leap/leap/LeapControllerListener.cs
@@ -11,15 +11,30 @@ namespace leap
     {
         // Controller Frame Listener
         MotionFuntion mf = new MotionFuntion();
+        int mainHandID = 0;
         public void OnFrame(object sender, FrameEventArgs args) // 모션 인식 후, 동작 (마우스 이동같이)
         {
-            Frame frame = args.frame;
-            foreach(Hand hand in frame.Hands)
-            {
-                mf.grab(frame);
+            //foreach (Hand hand in args.frame.Hands)
+            //{
+            //    if (mainHandID < hand.Id)
+            //        mainHandID = hand.Id;
 
+            //}
+
+
+            try
+            {
+                Frame frame = args.frame;
+                if (frame.Hands.Count > 0)
+                {
+                    mf.grab(frame);
+                    if (frame.Hands.Count == 2)
+                    {
+
+                    }
+                }
             }
-            
+            catch { }
         }
 
         // Device connection check listener
